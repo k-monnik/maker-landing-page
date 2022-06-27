eecb8918b72bb691979f1a823f5623efe5170a50
Kristen Monnik <nolaviolinist@gmail.com>
2022-06-26 21:03:05 -0400

    add screen size and color customizations to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1440px'
+    },
+    extend: {
+      colors: {
+        'teal': '#3EE9E5',
+        'ocean': '#093F68',
+        'grey': '#777F98',
+        'white': '#FFFFFF',
+        'navy': '#080C20',
+        'pink': '#FF2965',
+      }
+    },
   },
   plugins: [],
 }
