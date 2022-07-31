00703b8a2e3e2b8bce315a3438dff1285b3340ff
Kristen Monnik <nolaviolinist@gmail.com>
2022-07-30 21:05:21 -0400

    add changes to validateEmail function to specify click event and adjust border styling based on input value

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
+// document.getElementById("registerEmail").addEventListener("submit", (event) => {
+//     document.getElementById('message').style.display = 'none';
+//     let email = document.getElementById("email").value;
+//     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     if (document.getElementById("email").value === "") {
+//         event.preventDefault();
+//         return (email.match(validRegex) ? alert('Thank you') : document.getElementById('message').style.display = 'block')
+//     }
+// });
+
+const emailBtn = document.getElementById("button");
+emailBtn.addEventListener("click", validateEmail);
+
 function validateEmail() {
     document.getElementById('message').style.display = 'none';
     let email = document.getElementById("email").value;
     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    event.preventDefault();
-    return (email.match(validRegex) ? alert('Thank you') : document.getElementById('message').style.display = 'inline')
-    // {
-
-
-
-
-
-    //     return true;
-
-    // } else {
-
-    //     document.getElementById('message').style.display = 'inline';
-    //     setTimeout(3000);
-
-
-
-    //     return false;
-
-    // }
+    // e.preventDefault();
+    return (email.match(validRegex) ? alert('Thank you') : document.getElementById('message').style.display = 'block')
 
 }
+
+
+// & (document.getElementById("registerEmail").reset())
+// function resetForm(){
+//     if validateEmail
+// }
+
+// document.getElementById("email").reset();
