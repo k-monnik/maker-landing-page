4cc871b899c3febcc5089ee7c8faaf974c5ac659
Kristen Monnik <nolaviolinist@gmail.com>
2022-07-30 21:54:06 -0400

    add resetForm function to clear validation error message, delete unnecessary code from app.js

--- src/app.js
+++ src/app.js
@@ -1,29 +1,24 @@
-// document.getElementById("registerEmail").addEventListener("submit", (event) => {
-//     document.getElementById('message').style.display = 'none';
-//     let email = document.getElementById("email").value;
-//     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-//     if (document.getElementById("email").value === "") {
-//         event.preventDefault();
-//         return (email.match(validRegex) ? alert('Thank you') : document.getElementById('message').style.display = 'block')
-//     }
-// });
 
 const emailBtn = document.getElementById("button");
 emailBtn.addEventListener("click", validateEmail);
+
+const emailInput = document.getElementById("email");
+emailInput.addEventListener("click", resetForm());
 
 function validateEmail() {
     document.getElementById('message').style.display = 'none';
     let email = document.getElementById("email").value;
     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     // e.preventDefault();
-    return (email.match(validRegex) ? alert('Thank you') : document.getElementById('message').style.display = 'block')
+    return (email.match(validRegex) ? alert('Thank you!') : document.getElementById('message').style.display = 'block');
 
 }
 
+function resetForm() {
+    let email = document.getElementById("email").value;
+    if (email === "") {
+        return document.getElementById('message').style.display = 'none';
+    }
 
-// & (document.getElementById("registerEmail").reset())
-// function resetForm(){
-//     if validateEmail
-// }
+}
 
-// document.getElementById("email").reset();
